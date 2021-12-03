e71f6da63fabc9a63a0f46b9aaa5e7399684785b
dontknow945 <a0978878897@gmail.com>
2021-12-04 00:24:08 +0800

    complete the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
+    const len = str.length;
+    let result = "";
 
+    for (let i=len-1; i>=0; i--) {
+        result += str[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
