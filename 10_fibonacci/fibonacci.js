06da4e1a3e683f78ce2dfb879725145fbcac688d
dontknow945 <a0978878897@gmail.com>
2021-12-13 22:22:04 +0800

    complete the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let first = 1,
+        second = 1,
+        temp = 0;
 
+    if (num < 0) return "OOPS";
+    
+    for (let i=2; i<num; i++) {
+        temp = second;
+        second += first;
+        first = temp;
+    }
+    return second;
 };
 
 // Do not edit below this line
