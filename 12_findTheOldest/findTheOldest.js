b77b700f5203cd626d083f74d6391dee1a0eb028
dontknow945 <a0978878897@gmail.com>
2021-12-13 22:50:05 +0800

    complete the exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((a, b) => {
+        if (!("yearOfDeath" in a)) a.yearOfDeath = new Date().getFullYear();
+        if (!("yearOfDeath" in b)) b.yearOfDeath = new Date().getFullYear();
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        return aAge > bAge ? a : b;
+    });
 };
 
 // Do not edit below this line
