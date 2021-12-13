ec5e1bad95a843f0ffac40ee248be9c7e3a0ea2a
dontknow945 <a0978878897@gmail.com>
2021-12-13 22:09:52 +0800

    complete the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = str.match(/[a-zA-Z]/g).join('').toLowerCase();
+    let len = newStr.length;
+    for (let i=0; i<len/2; i++) {
+        if (newStr.charAt(i) != newStr.charAt(len-1-i)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
